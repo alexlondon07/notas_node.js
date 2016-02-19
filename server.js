cd475ba0e68a17157426a2e38fce50dbd68b94b7
Alexander Andrès Londoño Espejo <alexander.londono@secuencia24.com>
2016-02-18 20:47:28 -0500

    Modularizacion the  código

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 * Variables locales
 */
 var server = express();
-var db = {};
+
 
 /**
 * Middleware
@@ -18,40 +18,15 @@ server.use(bodyParser.json('application/json'))
 /**
 * Rutas
 */
-server.post('/notas', function(req, res){
-  console.log('POST', req.body.nota);
-  //Manipulate request
-  var notaNueva = req.body.nota;
-  notaNueva.id = Date.now();
-
-  //Almacenando la nota
-  db[notaNueva.id]=notaNueva;
-
-  res.set('Content-Type', 'application/json');
-  res.status(201);
-  //Send response
-  res.json({
-      nota:notaNueva
-    });
-});
-
-server.get('/notas/:id?', function(req, res){
-  console.log('GET /notas/%s', req.params.id);
-  var id = req.params.id;
-  var nota = db[id];
-
-  res.json({
-    notas: nota
-  });
-
-});
+var notas = require ('./lib/notas');
+server.use.notas;
 
 /**
 * Poner en marcha nuestro server
 */
 if(!module.parent){
   server.listen(3000, function(){
-    console.log('Hola estoy Aprendiendo Node.js');
+    console.log('Hola estoy Aprendiendo Node.js http://localhost:3000/notas/');
   });
 }else {
   module.exports=server;
